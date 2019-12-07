d443a26bc9ee9507956a1054100ea9ccb21dc11d
Nacho Abad <liao2512@gmail.com>
2019-12-07 21:33:11 +0100

    adds users seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+if Rails.env.development?
+  AdminUser.create!(email: 'admin1@mail.com', password: 'adminadmin', password_confirmation: 'adminadmin')
+
+  user1 = User.create! email: 'user1@mail.com', password: 'useruser', password_confirmation: 'useruser'
+  user2 = User.create! email: 'user2@mail.com', password: 'useruser', password_confirmation: 'useruser'
+end
